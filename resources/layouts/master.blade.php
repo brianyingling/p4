1bdc4d6b024258994cedb86c7ec6b54bbc46cdd4
Brian Yingling <brian@jet.com>
2018-04-22 11:05:18 -0400

    included bootstrap

--- resources/layouts/master.blade.php
+++ resources/layouts/master.blade.php
@@ -1,6 +1,9 @@
 <html>
     <head>
-        <title></title>
+        <title>Project 4: Movie Message Boards</title>
+        <meta charset='utf-8'>
+        <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' type='text/css'>
+        <link rel="stylesheet" href='css/styles.css'>
     </head>
 
     <body>
