029aa7c6ca510179e4743a27bddf081d8603820d
Brian Yingling <brian@jet.com>
2018-04-22 11:05:45 -0400

    stubbing out application

--- resources/layouts/master.blade.php
+++ resources/layouts/master.blade.php
@@ -5,9 +5,8 @@
         <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' type='text/css'>
         <link rel="stylesheet" href='css/styles.css'>
     </head>
-
+    
     <body>
-        
+        <h1>Stubbing out Application</h1>
     </body>
-
 </html>
