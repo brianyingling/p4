71084c04103001cfd51804987b9f9ced1820ac20
Brian Yingling <brian@jet.com>
2018-05-07 23:57:37 -0400

    x-frame-options

--- app/Http/Middleware/FrameHeadersMiddleware.php
+++ app/Http/Middleware/FrameHeadersMiddleware.php
@@ -16,7 +16,7 @@ class FrameHeadersMiddleware
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        $response->header('X-Frame-Options', 'ALLOW FROM https://p4.byinglingdwa15.site/');
+        $response->header('X-Frame-Options', 'DENY');
         return $response;
     }
 }
